e4b2f92de97487025aa63b72cc1663f4e09d654d
sattu lahari <sattulahari@gmail.com>
2021-10-27 22:59:25 -0700

    wild cards

---
+++ DXC/src/generics/wildcards/Circle.java
@@ -0,0 +1,13 @@
+package generics.wildcards;
+
+public class Circle extends Shape{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@Override
+		void drawShape() {
+			System.out.println("drawing circle");
+
+	}
+
+}
